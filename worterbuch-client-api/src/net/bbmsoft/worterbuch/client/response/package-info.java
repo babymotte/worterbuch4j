bd095f4af1adbea1163421ad0923aa59b2cc42de
Michael Bachmann <mbachmann@bbmsoft.net>
2025-03-28 17:26:48 +0100

    13.1.0

--- worterbuch-client-api/src/net/bbmsoft/worterbuch/client/response/package-info.java
+++ worterbuch-client-api/src/net/bbmsoft/worterbuch/client/response/package-info.java
@@ -1,4 +1,3 @@
 @org.osgi.annotation.bundle.Export
-@org.osgi.annotation.versioning.Version("1.0.0")
+@org.osgi.annotation.versioning.Version("1.1.0")
 package net.bbmsoft.worterbuch.client.response;
-
