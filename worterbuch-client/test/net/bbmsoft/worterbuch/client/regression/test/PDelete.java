80a040b1dd2b5865e13c71818b76ce2972553b49
Michael Bachmann <mbachmann@bbmsoft.net>
2025-04-04 17:28:40 +0200

    added tests

--- worterbuch-client/test/net/bbmsoft/worterbuch/client/regression/test/PDelete.java
+++ worterbuch-client/test/net/bbmsoft/worterbuch/client/regression/test/PDelete.java
@@ -60,4 +60,73 @@ public class PDelete {
 				PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).err().getErrorCode());
 
 	}
+
+	@Test
+	public void singleWildcardValuesGetDeletedQuietly()
+			throws NoSuchElementException, WorterbuchException, InterruptedException, TimeoutException {
+
+		final var key = "pDelete/singleWildcardValuesGetDeletedQuietly/test/1";
+		final var pattern = "pDelete/singleWildcardValuesGetDeletedQuietly/?/1";
+		final var value = "123";
+
+		PDelete.WB.client.set(key, value);
+		Assert.assertEquals(value, PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).value());
+
+		Assert.assertNull(PDelete.WB.client.pDelete(pattern).await(Duration.ofMillis(100)).value());
+		Assert.assertEquals(ErrorCode.NoSuchValue,
+				PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).err().getErrorCode());
+
+	}
+
+	@Test
+	public void singleWildcardValuesGetDeleted()
+			throws NoSuchElementException, WorterbuchException, InterruptedException, TimeoutException {
+
+		final var key = "pDelete/singleWildcardValuesGetDeleted/test/1";
+		final var pattern = "pDelete/singleWildcardValuesGetDeleted/?/1";
+		final var value = "123";
+
+		PDelete.WB.client.set(key, value);
+		Assert.assertEquals(value, PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).value());
+
+		PDelete.WB.client.pDelete(pattern, String.class).await(Duration.ofMillis(100));
+		Assert.assertEquals(ErrorCode.NoSuchValue,
+				PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).err().getErrorCode());
+
+	}
+
+	@Test
+	public void multiWildcardValuesGetDeletedQuietly()
+			throws NoSuchElementException, WorterbuchException, InterruptedException, TimeoutException {
+
+		final var key = "pDelete/multiWildcardValuesGetDeletedQuietly/test/1";
+		final var pattern = "pDelete/multiWildcardValuesGetDeletedQuietly/#";
+		final var value = "123";
+
+		PDelete.WB.client.set(key, value);
+		Assert.assertEquals(value, PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).value());
+
+		Assert.assertNull(PDelete.WB.client.pDelete(pattern).await(Duration.ofMillis(100)).value());
+		Assert.assertEquals(ErrorCode.NoSuchValue,
+				PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).err().getErrorCode());
+
+	}
+
+	@Test
+	public void multiWildcardValuesGetDeleted()
+			throws NoSuchElementException, WorterbuchException, InterruptedException, TimeoutException {
+
+		final var key = "pDelete/multiWildcardValuesGetDeleted/test/1";
+		final var pattern = "pDelete/multiWildcardValuesGetDeleted/#";
+		final var value = "123";
+
+		PDelete.WB.client.set(key, value);
+		Assert.assertEquals(value, PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).value());
+
+		PDelete.WB.client.pDelete(pattern, String.class).await(Duration.ofMillis(100));
+		Assert.assertEquals(ErrorCode.NoSuchValue,
+				PDelete.WB.client.get(key, String.class).await(Duration.ofMillis(100)).err().getErrorCode());
+
+	}
+
 }
